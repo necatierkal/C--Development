12c6a32938ef255f977f347c57d0f81cfc296731
necatierkal <76514144+necatierkal@users.noreply.github.com>
2025-01-11 23:53:35 +0300

    Add

---
+++ StaticsDemo/MsbList.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticsDemo;
+
+internal class MsbList
+{
+    public static int MaxElemanSayi { get; set; } = 100;
+
+    public int ElemanSayisi { get; }
+
+    public void Ekle(object eleman)
+    {
+        if (ElemanSayisi >= MaxElemanSayi)
+            throw new Exception("Liste max eleman sayısına ulaşmıştır");
+    }
+}
---
+++ StaticsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using StaticsDemo;
+
+//var mathHelper = new MathHelper();
+//mathHelper.OrtalamaAl(2,3);
+
+var ortSonuc = MathHelper.OrtalamaAl(2, 3);
+var ortSonuc2 = MathHelper.OrtalamaAl(2, 3);
+var ortSonuc3 = MathHelper.OrtalamaAl(2, 3);
+
+var piSayisi = MathHelper.PiSayisiGetir;
+
+MsbList.MaxElemanSayi = 200;
+
+var isimler = new MsbList();
+//isimler.MaxElemanSayi = 200;
+isimler.Ekle("Salih");
+
+Console.WriteLine("Bitti");
